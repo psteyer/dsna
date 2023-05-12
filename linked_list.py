81b1df1ce4b35a532622cbdec82e3823ecaadb23
Udo-Peter Steyer <35242265+psteyer@users.noreply.github.com>
2023-05-12 12:52:02 -0500

    Add files via upload
    init commit

---
+++ linked_list.py
@@ -0,0 +1,111 @@
+# What is a link list?
+# A collection of nodes, where each node has two attributes, data it holds and the pointer to the next node
+# What is a node?
+# A container for some sort of data
+
+# the constructor for a class in py is __init__
+
+"""
+Disadvantage
+    - slow to get to the xth element
+        - o(n) -> linear
+
+Advantage
+    - insert and delete can be quick
+        - o(1) prepend
+        - o(n) append
+    
+"""
+
+# Node class:
+class Node():
+    # Constructor/initilization:
+    # pointer is of default None, so when at the end of the list it points no where
+    def __init__(self, data, pointer = None):
+        self.data = data
+        self.pointer = pointer
+
+
+
+class Linklist:
+    # init is a constructor that instatiates an object and sets instance attributes.
+    def __init__(self, head_node):
+        # head node is an instance attribute.
+        # instance attribute: Per instance this attribute is different
+        self.head_node = head_node
+
+
+    def search(self, data):
+        """
+        Search wants to see if a value lives anywhere inside of a link list
+        Inside of all the nodes do you have one node, or any amount of node - that contain this data I'm sending you
+
+
+        parameters:
+            some sort of data type
+                string, number, whatever you give it. Just not a node
+
+        return value:
+            either a node or False
+            easier: return True or False
+
+
+        (head, my data- yes or no) -> (data: yes or no) ->....->(tail: yes or no; return false)
+        """
+        current_node = self.head_node
+        while current_node.pointer:
+            if current_node.data == data:
+                return True
+            current_node = current_node.pointer
+        return current_node.data == data
+
+    def insert_tail(self, node):
+        current_node = self.head_node
+        # while the current nodes pointer points at the next node - we do this loop
+        # While Flase - I'm not gonna run the indented line, I move on.
+        # While some condition is true, we will run my block, once condition is false - we will move on to the next line
+        # while loop is always looking for something true
+        while current_node.pointer:
+            current_node = current_node.pointer
+        current_node.pointer = node
+
+    def delete():
+        pass
+
+    
+    def update(self, data, new_data):
+        """
+        Search for a value inside of the link list, and set that value to a new value
+        
+        Params:
+            value we are looking for
+            value we want to replace it with
+
+
+        return:
+            True or False
+            True if you are able to find and replace value
+            False if you were not able to find and replace value
+
+        """
+
+        current_node = self.head_node
+        while current_node.pointer:
+            if current_node.data == data:
+                current_node.data = new_data
+                return True
+            current_node = current_node.pointer 
+        if current_node.data == data:
+            current_node.data = new_data
+            return True
+        return False
+
+
+# (head:node:peter)->(tail:node:doggyhorse)->
+my_node = Node("peter")
+my_linklist = Linklist(my_node) #(peter)->
+my_tail_node = Node("doggyhorse")
+my_linklist.insert_tail(my_tail_node)#(peter)->(doggyhorse)->
+my_another_node = Node("horsecat")
+my_linklist.insert_tail(my_another_node)
+
