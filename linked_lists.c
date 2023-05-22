19c8f5e1a4b32332e96ff062a972c1c80de830cb
Udo-Peter Steyer <ous8292@gmail.com>
2023-05-22 14:44:43 -0500

    adding c version

---
+++ linked_lists.c
@@ -0,0 +1,27 @@
+
+#include <stio.h>
+
+
+typedef struct tagLL_NODE {
+    truct tagLL_NODE* prev;
+    truct tagLL_NODE* next;
+    int data;
+} LL_NODE;
+
+typedef struct LINKEDLIST {
+    LL_NODE* head;
+    LL_NODE* tail;
+} LINKEDLIST;
+
+
+void linkedlink(LINKEDLIST* linknked_list){
+
+}
+
+int main()
+{
+    int value = 0;
+    LINKEDLIST linked_list;
+    linked_list_initialize(&linked_list)
+	return 0;
+}
